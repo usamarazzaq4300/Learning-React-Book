120372060833cec082a0cb059e176d3a31c59ffb
unknown <usamarazzaq4300@hotmail.com>
2021-05-10 16:07:57 +0500

    Learning

--- src/state.js
+++ src/state.js
@@ -1,30 +1,30 @@
-import {useState} from "react"
-function State()
-{
+import { useState } from "react"
+import Button from '@material-ui/core/TableBody'
+function State() {
     // State in React
-    const[myName, setName]= useState("usama")
-    const[yourName, setyourName]=useState("noman")
-    const[flag, setFlag]=useState(true)
-    const [arr,setarr]= useState(["usama","sayaam","bakar"])
+    const [myName, setName] = useState("usama")
+    const [yourName, setyourName] = useState("noman")
+    const [flag, setFlag] = useState(true)
+    const [arr, setarr] = useState(["usama", "sayaam", "bakar"])
     return (
         <div>
-        <h2>Students List</h2>
-        <ul>
-        {
-            arr.map((names)=>{
-                return(
-                    <li>{names}</li>
-                )
-            })
-        }
-        </ul>
+            <h2>Students List</h2>
+            <ul>
+                {
+                    arr.map((names) => {
+                        return (
+                            <li>{names}</li>
+                        )
+                    })
+                }
+            </ul>
             {
                 flag ?
-                <p style={{background: flag? "red":"blue"}}>{myName}</p>
-                :
-                <p>{yourName}</p>
+                    <p style={{ background: flag ? "red" : "blue" }}>{myName}</p>
+                    :
+                    <p style={{ background: flag ? "red" : "blue" }}>{yourName}</p>
             }
-            <button onClick={()=> setFlag(!flag)}>Change Name</button>
+            <button onClick={() => setFlag(!flag)}>Change Name</button>
         </div>
     )
 }
